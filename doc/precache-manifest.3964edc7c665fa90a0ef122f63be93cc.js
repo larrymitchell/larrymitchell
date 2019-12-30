9c414ce8f991b2f268c004433c49203bff2a67a9
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-12-29 19:06:22 -0500

    Ran create-react-app

---
+++ doc/precache-manifest.3964edc7c665fa90a0ef122f63be93cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209a3e3e2e550da62105782467bb8d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d124543c6e0011cd340",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f51b33d841f23773e2cc",
+    "url": "/static/js/2.4c065fd3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4c065fd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d124543c6e0011cd340",
+    "url": "/static/js/main.b7f054a4.chunk.js"
+  },
+  {
+    "revision": "16771b5134a7c00f0269",
+    "url": "/static/js/runtime-main.5d5aad83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
