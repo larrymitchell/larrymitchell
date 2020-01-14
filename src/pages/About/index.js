8738b2fdcf48771c04c50ce1e6269483b51cff39
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-01-14 11:49:07 -0500

    Added Larry's contact information

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,6 +4,11 @@ const About = () => (
   <>
     <h2>Hi, I'm Lawrence Mitchell!</h2>
     <p>Designer and Creative</p>
+    <address>
+      <p>
+        You can contact me at <a href="tel:+2166780197">(216) 678-0197</a>
+      </p>
+    </address>
   </>
 );
 
